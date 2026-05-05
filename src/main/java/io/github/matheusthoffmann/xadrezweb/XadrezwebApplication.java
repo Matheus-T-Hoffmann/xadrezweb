fccf4b5b690aab6cc2d3264cb117f40f1b26d57c
Matheus Hoffmann <matheust.hoffmann@gmail.com>
2026-05-05 01:13:41 -0300

    Initial project

---
+++ src/main/java/io/github/matheusthoffmann/xadrezweb/XadrezwebApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheusthoffmann.xadrezweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XadrezwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XadrezwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/matheusthoffmann/xadrezweb/XadrezwebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheusthoffmann.xadrezweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XadrezwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
